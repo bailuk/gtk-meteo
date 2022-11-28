cf0122282f9943a95ed6d3cad02c17d088172d8b
bailuk <bailu@bailu.ch>
2022-11-28 20:23:14 +0100

    remove plugin

--- build.gradle.kts
+++ build.gradle.kts
@@ -1,9 +1,6 @@
 plugins {
     application
 
-    // https://imperceptiblethoughts.com/shadow/getting-started
-    id("com.github.johnrengelman.shadow") version "7.1.2"
-
     // https://kotlinlang.org/docs/gradle.html#targeting-the-jvm
     kotlin("jvm") version "1.7.21"
 }
