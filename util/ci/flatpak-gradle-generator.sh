4380264e1462c85f72dd1dc44f8ad808bafeee53
bailuk <bailu@bailu.ch>
2022-11-28 21:10:59 +0100

    Add plugin and regenerate source list

--- util/ci/flatpak-gradle-generator.sh
+++ util/ci/flatpak-gradle-generator.sh
@@ -16,15 +16,23 @@ set -e
 
 # You may want to modify these variables
 # Where the sources json file should be generated:
-SOURCES_FILE="flatpak/gradle-sources.json"
+
+test -f gradlew || cd ..
+test -f gradlew || cd ..
+test -f gradlew || exit 1
+
+PROJECT_ROOT="$(pwd)"
+
+SOURCES_FILE="${PROJECT_ROOT}/flatpak/gradle-sources.json"
 
 # The target to generate the dependencies for:
 TARGET="build"
+
 # The maven repos:
 REPO_BASEURL=(
+  'https://plugins.gradle.org/m2/'
 	'https://repo1.maven.org/maven2/'
 	'https://jitpack.io/'
-	'https://plugins.gradle.org/m2/'
 )
 
 # Manually specify dependencies that aren't getting cached for some reason:
@@ -34,21 +42,15 @@ MANUAL_ARTIFACTS=(
 )
 
 
-echo "These directories should be empty"
-gradle_user_home="build/gradle_flatpak"
-maven_repo="build/maven_flatpak"
+gradle_user_home="${PROJECT_ROOT}/build/gradle_flatpak"
+maven_repo="${PROJECT_ROOT}/build/maven_flatpak"
 
 mkdir -p $gradle_user_home
 mkdir -p $maven_repo
 
-wd="$(pwd)"
-
 # Let gradle fetch all the dependencies into a new clean gradle user home:
-echo "Downloading all dependencies..."
+echo "Downloading all dependencies into ${gradle_user_home}"
 ./gradlew -Dhttp.proxyHost=127.0.0.1 -Dhttp.proxyPort=3128 -Dhttps.proxyHost=127.0.0.1 -Dhttps.proxyPort=3128 -g "$gradle_user_home" "$TARGET" --no-daemon -q
-# --dry-run seems to miss some dependendencies
-# ./gradlew -g "$gradle_user_home" "$TARGET" --no-daemon --dry-run -q
-
 
 cd "$gradle_user_home/caches/modules-2/files-2.1" || exit 1
 
@@ -58,29 +60,26 @@ cd "$gradle_user_home/caches/modules-2/files-2.1" || exit 1
 
 echo "Transforms gradle cache paths to maven repo paths"
 function mavenize {
-	IFS='/' read -r -a paths <<< "$1"
+  IFS='/' read -r -a paths <<< "$1"
 	groupId=$(echo "${paths[1]}" | tr . /)
 	artifactId="${paths[2]}"
 	version="${paths[3]}"
 	echo "$groupId/$artifactId/$version"
 }
 
-echo $(pwd)
 echo "Copy every file from the cache to it's maven repo location"
 find . -type f -print0 | while IFS= read -r -d '' file; do
 	filename=$(basename "$file")
 	source_dir=$(dirname "$file")
-	echo "mavenize $file -> $(mavenize "$file")"
-	target_dir="$wd/$maven_repo/$(mavenize "$file")"
-	echo "$source_dir/$filename -> $target_dir/"
+	target_dir="$maven_repo/$(mavenize "$file")"
 	mkdir -p "$target_dir" && cp "$source_dir/$filename" "$target_dir/"
 done
 
 echo "All interesting files are now in the maven repo"
 echo "Create the json sources file"
-cd "$wd/$maven_repo"
+cd "$maven_repo"
 
-json_file="$wd/$SOURCES_FILE"
+json_file="$SOURCES_FILE"
 echo '[' > "$json_file"
 
 echo "Download the $MANUAL_ARTIFACTS manually"
@@ -115,9 +114,9 @@ find * -type f -print0 | while IFS= read -r -d '' file; do
 	done
 	if [ -z "$url" ]; then
 		echo "ERROR: No repo contains $file"
-		# exit 1
+		exit 1
 	fi
-
+  echo "$url -> $file ($hash)"
 	hash="$(sha256sum "$file" | cut -f 1 -d ' ')"
 	cat << HERE >> "$json_file"
 	{
@@ -138,6 +137,6 @@ echo '	}' >> "$json_file"
 echo ']' >> "$json_file"
 
 # Clean up maven repo too
-cd "$wd"
+cd "$projectRoot"
 
 echo 'Finished. Success is unknown until observed.'
