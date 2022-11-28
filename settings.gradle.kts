5e71c7633c7b9b84ad4239b6b89dbdeba426bbe8
bailuk <bailu@bailu.ch>
2022-11-28 20:19:32 +0100

    plugin management

--- settings.gradle.kts
+++ settings.gradle.kts
@@ -1,6 +1,7 @@
 pluginManagement {
     repositories {
         gradlePluginPortal()
+        maven ("local_maven")
         flatDir { // precached artifacts for flatpak build TODO change name
             dirs("local_maven")
         }
